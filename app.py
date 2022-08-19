6286fda7d1ba7e08e1e896042299d1780abc0737
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2022-08-19 15:11:30 +0200

    Update app.py
    Changed code to use a database instead of storing data internally with a list

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-task_list = ["Have lunch", "Write code", "Date night"]
+db = SQLAlchemy(app)
+
+
+class TaskList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(100), index=True)
+
+    def __repr__(self):
+        return f"{self.text}"
+
+
+#db.create_all()
+
 
 # creating form object
 class TaskForm(FlaskForm):
@@ -18,7 +33,8 @@ def index():
     # accepting new tasks
     # if there is a 'task' attribute in the form
     if 'task' in request.form:
-        task_list.append(request.form['task'])
+        db.session.add(TaskList(text=request.form['task']))
+        db.session.commit()
 
         # template variables will be list with tasks and a new form object
-    return render_template('index.html', tasks=task_list, task_form=TaskForm())
+    return render_template('index.html', tasks=TaskList.query.all(), task_form=TaskForm())
